859c8dee5aa2c7c9980c00ba50418105f09f7850
Erick <erickvillaseor@yahoo.com.mx>
2025-07-03 19:41:55 -0600

    Se creo el put y delete en el controlador de genero

--- libreria/Controllers/GeneroController.cs
+++ libreria/Controllers/GeneroController.cs
@@ -30,4 +30,38 @@ public class GeneroController : ControllerBase
         await _context.SaveChangesAsync();
         return CreatedAtAction("GetGenero", new { id = genero.IdGenero }, genero);
     }
+
+    // PUT: api/Genero/id
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutGenero(int id, Genero genero)
+    {
+        if (id != genero.IdGenero) return BadRequest();
+        _context.Entry(genero).State = EntityState.Modified;
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!GeneroExists(id)) return NotFound();
+            else throw;
+        }
+        return NoContent();
+    }
+
+    // DELETE: api/Genero/id
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteGenero(int id)
+    {
+        var genero = await _context.Generos.FindAsync(id);
+        if (genero == null) return NotFound();
+        _context.Generos.Remove(genero);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+
+    private bool GeneroExists(int id)
+    {
+        return _context.Generos.Any(e => e.IdGenero == id);
+    }
 }
