325f028f3499d00fba60f3b6e9982916f994939e
Erick <erickvillaseor@yahoo.com.mx>
2025-07-03 19:25:20 -0600

    Se creo el get y post en el controlador de genero

--- libreria/Controllers/GeneroController.cs
+++ libreria/Controllers/GeneroController.cs
@@ -1,6 +1,33 @@
-﻿namespace libreria.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using libreria.Models;
+
+namespace libreria.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class GeneroController : ControllerBase
 {
-    public class GeneroController
+    private readonly DBLibreriaContext _context;
+
+    public GeneroController(DBLibreriaContext context)
     {
+        _context = context;
+    }
+
+    // GET: api/Genero
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Genero>>> GetGeneros()
+    {
+        return await _context.Generos.ToListAsync();
+    }
+
+    // POST: api/Genero
+    [HttpPost]
+    public async Task<ActionResult<Genero>> PostGenero(Genero genero)
+    {
+        _context.Generos.Add(genero);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction("GetGenero", new { id = genero.IdGenero }, genero);
     }
 }
