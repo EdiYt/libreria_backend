d2476050ea39b309994213065ac0593cdd6537b9
Erick <erickvillaseor@yahoo.com.mx>
2025-07-03 19:13:57 -0600

    Se creo el put y delete para autores

--- libreria/Controllers/AutorController.cs
+++ libreria/Controllers/AutorController.cs
@@ -30,4 +30,38 @@ public class AutorController : ControllerBase
         await _context.SaveChangesAsync();
         return CreatedAtAction("GetAutor", new { id = autor.IdAutor }, autor);
     }
+
+    // PUT: api/Autor/id
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutAutor(int id, Autor autor)
+    {
+        if (id != autor.IdAutor) return BadRequest();
+        _context.Entry(autor).State = EntityState.Modified;
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!AutorExists(id)) return NotFound();
+            else throw;
+        }
+        return NoContent();
+    }
+
+    // DELETE: api/Autor/id
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAutor(int id)
+    {
+        var autor = await _context.Autores.FindAsync(id);
+        if (autor == null) return NotFound();
+        _context.Autores.Remove(autor);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+
+    private bool AutorExists(int id)
+    {
+        return _context.Autores.Any(e => e.IdAutor == id);
+    }
 }
